27845e8fc1d8b2f5e4ba0c0f6a841bac54616525
AnastasiyaLytvyn <107053207+AnastasiyaLytvyn@users.noreply.github.com>
2023-03-16 21:39:01 +0200

    add task

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 
-<<<<<<< HEAD
 dotenv.config({ path: "./.env" });
 
 const port = process.env.PORT || 4000;
@@ -9,8 +8,3 @@ const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server running. Use our API on port: ${port}`);
 });
-=======
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000!")
-})
->>>>>>> d9b914df5906d3e03c7a15dd38331283879d5380
